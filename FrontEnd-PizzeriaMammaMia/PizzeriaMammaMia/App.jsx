a8f05500a69c5aa6c57a89d8a74e435bb2f74766
xpinayadasilva <xpinayads@gmail.com>
2025-01-23 00:43:55 -0400

    Hito 4 primer version

---
+++ FrontEnd-PizzeriaMammaMia/PizzeriaMammaMia/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './pages/Home'; 
+import Footer from './components/Footer'; 
+import Pizza from "./components/Pizza";
+/* 
+import LoginPage from './components/LoginPAge';  
+import RegisterPage from './components/RegisterPage'; 
+*/ 
+/* import Cart from './components/Cart'; */
+
+function App(){
+     
+  return (    
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <main className='flex-grow'>
+        {/* <Home /> */}
+        {/* <RegisterPage /> */}
+        {/* <LoginPage />  */}
+        {/* <Cart /> */}
+        <Pizza />
+      </main>
+      <Footer />     
+    </div>       
+  )
+}
+  
+
+export default App;
